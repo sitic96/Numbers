e26009dbbc21267fbc37bcd3c0e852c909a8d79a
Sitora Guliamova <redastify@gmail.com>
2023-08-07 22:43:55 +0300

    main: project setup

---
+++ Numbers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Numbers
+//
+//  Created by Sitora Guliamova on 8/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
